cb210b865d2521243c77f5bcbad7f53474d3d59e
Petr Hron <hronpet1@fit.cvut.cz>
2019-09-11 15:01:39 -0500

    Added entree classes

---
+++ Menu/Entrees/Entree.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public abstract class Entree
+    {
+        public double Price { get; set; }
+        public int Calories { get; set; }
+
+    }
+}
---
+++ Menu/Entrees/TRexKingBurger.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class TRexKingBurger : Entree
+    {
+        private bool wholeWheatBun = true;
+        private int steakburgerPatties = 3;
+        private bool lettuce = true;
+        private bool tomato = true;
+        private bool onion = true;
+        private bool pickle = true;
+        private bool ketchup = true;
+        private bool mustard = true;
+        private bool mayo = true;
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { };
+                if (wholeWheatBun) ingredients.Add("Whole Wheat Bun");
+                for ( int i= 0; i < steakburgerPatties; i++)
+                    ingredients.Add("Steakburger Pattie");
+                if (lettuce) ingredients.Add("Lettuce");
+                if (tomato) ingredients.Add("Tomato");
+                if (onion) ingredients.Add("Onion");
+                if (pickle) ingredients.Add("Pickle");
+                if (ketchup) ingredients.Add("Ketchup");
+                if (mustard) ingredients.Add("Mustard");
+                if (mayo) ingredients.Add("Mayo");
+                return ingredients;
+            }
+        }
+
+        public TRexKingBurger()
+        {
+            this.Price = 5.36;
+            this.Calories = 498;
+        }
+
+        public void HoldBun()
+        {
+            this.wholeWheatBun = false;
+        }
+       
+        public void HoldLettuce()
+        {
+            this.lettuce = false;
+        }
+
+        public void HoldTomato()
+        {
+            this.tomato = false;
+        }
+
+        public void HoldMayo()
+        {
+            this.mayo = false;
+        }
+
+        public void HoldPickle()
+        {
+            this.pickle = false;
+        }
+
+        public void HoldKetchup()
+        {
+            this.ketchup = false;
+        }
+        
+        public void HoldMustard()
+        {
+            this.mustard = false;
+        }
+
+        public void HoldOnion()
+        {
+            this.onion = false;
+        }
+
+    }
+}
