5e2663932ba3c65472fe800cc9c756c231d8a8ba
Petr Hron <hronpet1@fit.cvut.cz>
2019-09-27 14:42:36 -0500

    Added Combo class

---
+++ Menu/CretaceousCombo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Entrees;
+using DinoDiner.Menu.Sides;
+using DinoDiner.Menu.Drinks;
+using System.Linq;
+
+namespace DinoDiner.Menu
+{
+    public class CretaceousCombo
+    {
+        public Entree Entree { get; set; }
+        public Drink Drink { get; set; }
+        public Side Side { get; set; }
+
+        public CretaceousCombo()
+        {
+            Entree = new SteakosaurusBurger();
+            Side = new Fryceritops();
+            Drink = new Sodasaurus();
+        }
+
+        public CretaceousCombo(Entree entree, Drink drink, Side side)
+        {
+            Entree = entree;
+            Drink = drink;
+            Side = side;
+        }
+
+        private Size size = Size.Small;
+        public Size Size
+        {
+            get { return size; }
+            set
+            {
+                size = value;
+                Drink.Size = value;
+                Side.Size = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return Drink.Price + Side.Price + Entree.Price - 0.25;
+            }
+        }
+        public uint Calories
+        {
+            get
+            {
+                return Drink.Calories + Side.Calories + Entree.Calories;
+            }
+        }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> comboIngredients = new List<string>();
+                comboIngredients.Concat(Side.Ingredients);
+                comboIngredients.Concat(Entree.Ingredients);
+                comboIngredients.Concat(Drink.Ingredients);
+                return comboIngredients;
+
+            }
+        }
+        /// <summary>
+        /// TO DO
+        /// </summary>
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
---
+++ MenuTest/ComboTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DinoDiner.Menu;
+
+
+namespace MenuTest
+{
+    public class ComboTest
+    {
+        [Fact]
+        public void ComboMustBeSuppliedParameters()
+        {
+            // TO DO
+        }
+
+    }
+}
